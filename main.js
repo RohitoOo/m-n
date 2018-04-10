6029765084bac77ddb069e43e5df0a4875a0367c
RohitoOo <bhambhani.ba@gmail.com>
2018-04-10 14:55:54 +0530

    M^N -- Pending Time ommplexity

---
+++ main.js
@@ -0,0 +1,18 @@
+// Function of M Raised to the Power N
+
+//Only works whenvariable declared as 1
+var result = "1" ;
+//Only works whenvariable declared as 1
+var m = "1";
+
+function RaisedToThePower(2 , 2 ){
+
+for ( var i = 1 ; i <= n ; i ++ ) {
+
+  result = m * result;
+}
+
+return result
+}
+
+RaisedToThePower(2 , 2 );
